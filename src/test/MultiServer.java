b9c4ec2169f41b7fdf6d09a9eee8e96d33b4ee00
zzo2741 <dlwnstmd@gmail.com>
2020-05-18 17:50:06 +0900

    0518

--- src/test/MultiServer.java
+++ src/test/MultiServer.java
@@ -18,86 +18,67 @@ public class MultiServer {
 	static Socket socket = null;
 	Map<String, PrintWriter> clientMap;
 
-	public MultiServer()
-	{
+	public MultiServer() {
 		clientMap = new HashMap<String, PrintWriter>();
 		Collections.synchronizedMap(clientMap);
 	}
 
-	public void init()
-	{
+	public void init() {
 
-		try
-		{
+		try {
 			serverSocket = new ServerSocket(9999);
 			System.out.println("서버가 시작되었습니다.");
 
-			while (true)
-			{
+			while (true) {
 				socket = serverSocket.accept();
 				System.out.println(socket.getInetAddress() + ":" + socket.getPort());
 
-				if (clientMap.size() == 2)
-				{
+				if (clientMap.size() == 2) {
 					System.out.println("접속자 수 초과");
-					try
-					{ 
+					try {
 						socket.close();
-					} catch (Exception e)
-					{
+					} catch (Exception e) {
 						e.printStackTrace();
 					}
-				} else
-				{
+				} else {
 					Thread mst = new MultiServerT(socket);
 					mst.start();
 				}
 			}
 
-		} catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
-		} finally
-		{
-			try
-			{
+		} finally {
+			try {
 				serverSocket.close();
-			} catch (Exception e)
-			{
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
-	public void sendAllMsg(String name, String msg)
-	{
+	public void sendAllMsg(String name, String msg) {
 
 		Iterator<String> it = clientMap.keySet().iterator();
 
-		while (it.hasNext())
-		{
-			try
-			{
+		while (it.hasNext()) {
+			try {
 				PrintWriter it_out = (PrintWriter) clientMap.get(it.next());
 				msg = URLEncoder.encode(msg, "UTF-8");
 				name = URLEncoder.encode(name, "UTF-8");
 
-				if (name.equals(""))
-				{
+				if (name.equals("")) {
 					it_out.println(msg);
-				} else
-				{
+				} else {
 					it_out.println("[" + name + "] " + msg);
 				}
-			} catch (Exception e)
-			{
+			} catch (Exception e) {
 				System.out.println("예외:" + e);
 			}
 		}
 	}
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		MultiServer ms = new MultiServer();
 		ms.init();
 	}
@@ -108,28 +89,23 @@ public class MultiServer {
 		PrintWriter out = null;
 		BufferedReader in = null;
 
-		public MultiServerT(Socket socket)
-		{
+		public MultiServerT(Socket socket) {
 			this.socket = socket;
-			try
-			{
+			try {
 				out = new PrintWriter(this.socket.getOutputStream(), true);
 				in = new BufferedReader(new InputStreamReader(this.socket.getInputStream(), "UTF-8"));
-			} catch (Exception e)
-			{
+			} catch (Exception e) {
 				System.out.println("예외:" + e);
 			}
 		}
 
 		@Override
-		public void run()
-		{
+		public void run() {
 
 			String name = "";
 			String s = "";
 
-			try
-			{
+			try {
 				name = URLDecoder.decode(in.readLine(), "UTF-8");
 
 				sendAllMsg("", name + "님이 입장하셨습니다.");
@@ -138,8 +114,7 @@ public class MultiServer {
 				System.out.println(name + "> 접속");
 				System.out.println("현재 접속자 수는 " + clientMap.size() + "명 입니다.");
 
-				while (in != null)
-				{
+				while (in != null) {
 					s = URLDecoder.decode(in.readLine(), "UTF-8");
 					if (s == null)
 						break;
@@ -147,23 +122,19 @@ public class MultiServer {
 					System.out.println(name + " >> " + s);
 					sendAllMsg(name, s);//첫번째 인자 포함해서 메소드호출
 				}
-			} catch (Exception e)
-			{
+			} catch (Exception e) {
 				System.out.println("예외:" + e);
-			} finally
-			{
+			} finally {
 				clientMap.remove(name);
 				sendAllMsg("", name + "님이 퇴장하셨습니다.");
 				System.out.println(name + " [" + Thread.currentThread().getName() + "] 퇴장");
 				System.out.println("현재 접속자 수는 " + clientMap.size() + "명 입니다.");
 
-				try
-				{
+				try {
 					in.close();
 					out.close();
 					socket.close();
-				} catch (Exception e)
-				{
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
